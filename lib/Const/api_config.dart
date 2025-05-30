c8dfae1e425ff8606e1366129d7181796eb54e68
AnimeshCriterion <106810231+AnimeshCriterion@users.noreply.github.com>
2025-05-31 00:01:39 +0530

    inital commit

---
+++ lib/Const/api_config.dart
@@ -0,0 +1,6 @@
+class APIConfig {
+  static String domain = 'https://pos.apphouse.me/';
+  static String url = '${domain}api/v1';
+  static String registerUrl = '/sign-up';
+  static String businessCategoriesUrl = '/business-categories';
+}
